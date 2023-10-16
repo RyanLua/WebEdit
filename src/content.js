a6d6c20e7a7cf5f2f02c528a835a688cb23a3877
Ryan Luu <ryan.luu@ryanluu.dev>
2023-10-16 14:01:34 -0700

    Add comments to content.js
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/content.js
+++ src/content.js
@@ -1,5 +1,8 @@
+// Toggle content editable
 if (document.body.contentEditable === 'true') {
+    // Disable editing
     document.body.contentEditable = 'false';
 } else {
+    // Enable editing
     document.body.contentEditable = 'true';
 }
