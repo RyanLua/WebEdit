9dce515e25a94e4ae887551c21ac4920d80c7ffe
Ryan Luu <ryan.luu@ryanluu.dev>
2023-10-16 14:00:27 -0700

    Add state badge
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/background.js
+++ src/background.js
@@ -1,6 +1,26 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js']
+chrome.action.onClicked.addListener(async (tab) => {
+    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    // Next state will always be the opposite
+    const nextState = prevState === 'ON' ? '' : 'ON'
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
     });
+
+    if (nextState === "ON") {
+        // Enable editing when the user turns the extension on
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content.js']
+        });
+    } else if (nextState === "") {
+        // Stop editing when the user turns the extension off
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content.js']
+        });
+    }
 });
